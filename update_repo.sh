7b427c87b42f82e112b861d605d18af98f27122b
codego002 <codego002@gmail.com>
2026-04-08 20:07:12 +0300

    update_repo.sh: auto-generate Release with checksums

--- update_repo.sh
+++ update_repo.sh
@@ -11,7 +11,40 @@ if [ ! -s Packages ]; then
 fi
 
 echo "[*] Compressing..."
-gzip -kf Packages
+rm -f Packages.gz Packages.bz2
+gzip -k Packages
+bzip2 -k Packages
+
+echo "[*] Generating Release..."
+P_SIZE=$(wc -c < Packages)
+GZ_SIZE=$(wc -c < Packages.gz)
+BZ2_SIZE=$(wc -c < Packages.bz2)
+P_MD5=$(md5sum Packages | cut -d' ' -f1)
+GZ_MD5=$(md5sum Packages.gz | cut -d' ' -f1)
+BZ2_MD5=$(md5sum Packages.bz2 | cut -d' ' -f1)
+P_SHA256=$(sha256sum Packages | cut -d' ' -f1)
+GZ_SHA256=$(sha256sum Packages.gz | cut -d' ' -f1)
+BZ2_SHA256=$(sha256sum Packages.bz2 | cut -d' ' -f1)
+DATE=$(date -u "+%a, %d %b %Y %H:%M:%S UTC")
+cat > Release << EOF
+Origin: CodeGo Tweaks
+Label: CodeGo Tweaks
+Suite: stable
+Version: 1.0
+Codename: ios
+Architectures: iphoneos-arm64
+Components: main
+Description: CodeGo's iOS Tweaks & Apps Repository
+Date: $DATE
+MD5Sum:
+ $P_MD5 $P_SIZE Packages
+ $GZ_MD5 $GZ_SIZE Packages.gz
+ $BZ2_MD5 $BZ2_SIZE Packages.bz2
+SHA256:
+ $P_SHA256 $P_SIZE Packages
+ $GZ_SHA256 $GZ_SIZE Packages.gz
+ $BZ2_SHA256 $BZ2_SIZE Packages.bz2
+EOF
 
 echo "[*] Committing..."
 git add .
